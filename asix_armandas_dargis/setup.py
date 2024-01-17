f9cee8e2b86dee0b1a75f1929f3f399df9dc715f
Neapvilk <150431512+Neapvilk@users.noreply.github.com>
2024-01-17 20:54:57 +0100

    Update setup.py

---
+++ asix_armandas_dargis/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='asix_armandas_dargis',
+    version='0.1',
+    packages=find_packages(),
+    description='Paquete Python para cálculos de geometría',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Armandas',
+    url='https://github.com/Neapvilk/asix_armandas_dargis',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+)
